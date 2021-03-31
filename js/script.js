e27a6a7a7426b8b8c3766023114c8154b90ac67c
Jesse White <jesse.white6@gmail.com>
2021-03-31 16:23:21 -0700

    add function for vowel matching.
    co-authored-by: Min Chang <minchangmhc@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// ------Utility Logic-----
+const vowels = ["a", "e", "i", "o", "u"];
+
+
+
+// -----Business Logic-----
+
+function identifyAllVals(text) {
+    const wordArray = text.split(" ");
+
+    wordArray.forEach(function (element) {
+        const compareArray = element.split("");
+        for (let i = 0; i < vowels.length; i++) {
+            if (compareArray[0] === vowels[i]) {
+                element.concat("way")
+            }
+            return console.log(wordArray);
+        }
+    })
+
+}
+
+// -----User Interface Logic-----
+$(document).ready(function () {
+    $("#form").submit(function (event) {
+        event.preventDefault();
+        let passage = $("#passage").val();
+
+        $("#pigLatin").html(identifyAllVals(passage));
+
+    })
+})
